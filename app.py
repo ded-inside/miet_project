60718a3aaae859818b3c1b5485e8f4b625a4fa65
MTReload <suhonosik.dmitry@gmail.com>
2019-11-03 17:43:58 +0300

    implemented add_user

--- app.py
+++ app.py
@@ -13,9 +13,51 @@ from models import *
 db.create_all()
 
 
-@app.route('/')
+# """
+#     login: string
+#     password: string
+#
+#     return: token:string
+# """
+@app.route("/add/member", methods=['POST'])
+def add_member():
+    json = request.get_json()
+    if not json:
+        return abort(400)
+
+    login = json["login"]
+    password = json["password"]
+
+    if not login or not password:
+        return abort(400)
+
+    # todo add hash generation
+    password_hash = password[::-1]
+
+    member = db.session.query(Member).filter_by(login=login).first()
+    if member is not None:
+        return "login is already taken"
+
+    member = Member(login, password_hash)
+    db.session.add(member)
+    db.session.commit()
+    return "ok"
+
+
+@app.route('/test1')
 def hello_world():
-    return 'Hello World!'
+    member = Member("test")
+    member.password_hash = "123"
+    db.session.add(member)
+    db.session.commit()
+    return "ok"
+
+
+@app.route("/test2")
+def test():
+    a = Member.query.all()
+    member = a[-1]
+    return jsonify(login=member.login,id=member.id,pass_hash=member.password_hash)
 
 
 if __name__ == '__main__':
