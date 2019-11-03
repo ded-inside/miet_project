0276a25a88d631149630da9be4d713d11c62ba19
TheNooBest <mityankin@gmail.com>
2019-11-04 01:53:54 +0300

    Login page

--- app.py
+++ app.py
@@ -21,6 +21,12 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/login')
+def login():
+
+    return render_template('login.html');
+
+
 @app.route('/username')
 def username():
 
