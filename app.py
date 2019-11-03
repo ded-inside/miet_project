081307c368950b3336d08301d238fff1e7c010ec
MTReload <suhonosik.dmitry@gmail.com>
2019-11-03 16:44:39 +0300

    Implemented classes

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask import request
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:password@postgres:5432/postgres'
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
