166cb99a37564f50ab1a947fcfbe68145f078fc2
TheNooBest <mityankin@gmail.com>
2019-11-03 23:57:54 +0300

    Test commit, first page

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 from flask import Flask, jsonify, abort
-from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
+# from flask_sqlalchemy import SQLAlchemy
 from flask import request
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True
-app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:password@postgres:5432/postgres'
+# app.config['DEBUG'] = True
+# app.debug = True
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:password@postgres:5432/postgres'
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
-from models import *
-db.create_all()
+# from models import *
+# db.create_all()
 
 
 @app.route('/')
-def hello_world():
+def index():
 
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/username')
+def username():
+
+    user = { 'name': 'Dmitriy' }
+    return render_template('index.html', user = user)
 
 
 if __name__ == '__main__':
