b7c894002fcb5260bf05856a321698a6e0204647
MTReload <suhonosik.dmitry@gmail.com>
2019-11-17 21:01:54 +0300

    fixed deletes

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ from flask import request
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.debug = True
-
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:password@postgres:5432/postgres'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+db = SQLAlchemy(app)
+from models import *
+
+db.create_all()
 
 if __name__ == '__main__':
     print("LOL")
